a22b2b9041dccf1b5238abc878f097ef467f1700
PF <pierre-francois.gimenez@irit.fr>
2018-05-22 16:32:00 +0200

    Config comments

--- core/src/main/java/pfg/kraken/ConfigInfoKraken.java
+++ core/src/main/java/pfg/kraken/ConfigInfoKraken.java
@@ -32,7 +32,8 @@ import pfg.config.ConfigInfo;
 public enum ConfigInfoKraken implements ConfigInfo
 {
 	/**
-	 * Config du navmesh
+	 * Navmesh parameters
+	 * Require to restart the navmesh
 	 */
 	NAVMESH_OBSTACLE_DILATATION(100), // dilatation des obstacles dans le D* Lite.
 									// Comme c'est une heuristique, on peut
@@ -41,13 +42,18 @@ public enum ConfigInfoKraken implements ConfigInfo
 	LONGEST_EDGE_IN_NAVMESH(200), // in mm
 	NAVMESH_FILENAME("navmesh.krk"), // the filename of the navmesh
 	
+	/**
+	 * Autoreplanning parameters
+	 * Can be modified on-the-fly
+	 */
 	NECESSARY_MARGIN(40), // minimun distance that MUST be available in the current path, in mm
 	PREFERRED_MARGIN(60), // preferred distance that should be available in the current path, in mm
 	MARGIN_BEFORE_COLLISION(100), // maximal distance before the detected collision, in mm
 	INITIAL_MARGIN(100), // distance of the trajectory after the current point that can't be modified, in mm
 	
 	/**
-	 * Research and mechanical parameter
+	 * Mechanical parameter
+	 * Can be modified on-the-fly
 	 */
 	MAX_CURVATURE_DERIVATIVE(5), // maximal curvature derivative, in m⁻¹s⁻¹
 	MAX_LATERAL_ACCELERATION(3), // maximal lateral acceleration, in m/s²
@@ -57,21 +63,27 @@ public enum ConfigInfoKraken implements ConfigInfo
 	MAX_CURVATURE(5), // in m⁻¹
 	STOP_DURATION(800), // temps qu'il faut au robot pour s'arrêter et repartir
 						// (par exemple à cause d'un rebroussement) in ms
+	ALLOW_BACKWARD_MOTION(true), // allow the pathfinding to find a path with backward motion by default
+	
+	/**
+	 * Research parameters
+	 * Can be modified on-the-fly
+	 */
 	SEARCH_TIMEOUT(10000), // in ms
 	THREAD_NUMBER(1), // the number of threads for the tentacle computing. Recommended value for highest performance : nb cores + 1
-//	ALLOW_SPINNING(false), // can the robot spin ?
-	
-	ENABLE_DEBUG_MODE(false), // enable the debug mode
-	FAST_AND_DIRTY(false),
-	CHECK_NEW_OBSTACLES(false),
+	FAST_AND_DIRTY(false), // return the first path found, even if it's not the best one
+	CHECK_NEW_OBSTACLES(false), // check on the fly if there are new obstacles
 	
 	/**
 	 * Paramètres sur la gestion de la mémoire
+	 * Require to restart the pools
 	 */
-	NODE_MEMORY_POOL_SIZE(20000),
+	NODE_MEMORY_POOL_SIZE(20000), // nombre d'instances pour les nœuds
 	OBSTACLES_MEMORY_POOL_SIZE(50000), // nombre d'instances pour les obstacles
 			
-	ALLOW_BACKWARD_MOTION(true); // allow the pathfinding to find a path with backward motion by default
+
+	// à virer
+	ENABLE_DEBUG_MODE(false); // enable the debug mode
 
 	private Object defaultValue;
 	public volatile boolean uptodate;
